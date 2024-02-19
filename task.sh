f0daed1321766ce755abf4266bab2e5c6f7cb4e4
Dmytry-S <sviridoffd@gmail.com>
2024-02-19 14:24:45 +0200

    Implement Backup database task

--- task.sh
+++ task.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --no-create-db --result-file=backup-no-create-db.sql
+
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup-no-create-db.sql
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --no-create-db --no-create-info --result-file=backup-no-create-db_info.sql
+
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < backup-no-create-db_info.sql
+
