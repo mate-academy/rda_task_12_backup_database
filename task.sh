8c61a0378297fcbe11733ff109a965720e652b36
Yurii Smolii <45535903+YuriiSmolii@users.noreply.github.com>
2024-02-20 19:38:22 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,1 +1,8 @@
 #! /bin/bash
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --skip-add-drop-table --no-create-db --result-file=backup-no-create-db.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < backup-no-create-db.sql
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-info --result-file=backup-no-create-db_info.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < backup-no-create-db_info.sql
