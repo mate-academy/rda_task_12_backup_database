c919c5e11937f13af3a00083d5332b793e29f35f
kirillpolozenko <polozenko.kirill.job@gmail.com>
2024-03-13 01:30:17 +0200

    Fix

--- task.sh
+++ task.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-DB_USER=$DB_USER
-DB_PASSWORD=$DB_PASSWORD
+mysqldump  -u $DB_USER -p$DB_PASSWORD --skip-add-drop-table --no-create-db --result-file=full-backup.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < full-backup.sql
 
-mysqldump  -u $DB_USER -p$DB_PASSWORD --skip-add-drop-table --no-create-db --result-file=backup-no-create-db.sql
-mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup-no-create-db.sql
-
-mysqldump  -u $DB_USER -p$DB_PASSWORD  ShopDB --no-create-info --result-file=backup-no-create-db_info.sql
-mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < backup-no-create-db_info.sql
+mysqldump  -u $DB_USER -p$DB_PASSWORD  ShopDB --no-create-info --result-file=full-data-backup.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < full-data-backup.sql
