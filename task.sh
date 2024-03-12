57f6aeb12c34e953c097fad797596b62ea0fac4c
kirillpolozenko <polozenko.kirill.job@gmail.com>
2024-03-13 01:15:34 +0200

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+DB_USER=$DB_USER
+DB_PASSWORD=$DB_PASSWORD
+
+mysqldump  -u $DB_USER -p$DB_PASSWORD --skip-add-drop-table --no-create-db --result-file=backup-no-create-db.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup-no-create-db.sql
+
+mysqldump  -u $DB_USER -p$DB_PASSWORD  ShopDB --no-create-info --result-file=backup-no-create-db_info.sql
+mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < backup-no-create-db_info.sql
